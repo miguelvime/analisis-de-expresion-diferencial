9642da2443255cadef34ca7f4d6a5052632d825c
miguelvime <miguelvicentemeso@gmail.com>
2026-01-31 10:37:33 +0100

    Cambio nombre del archivo, continuo con normalizacion

--- expresion-diferencial-miguel-vicente.R
+++ expresion-diferencial-miguel-vicente.R
@@ -27,13 +27,25 @@ dim(mat) # Las dimensiones son 20500 genes (filas) x 1155 muestras (columnas)
 colnames(clinical)  
 dim(clinical) #114 variables clínicas
 
-# 3. Normalizo datos
+# 3. Normalizo datos (intersección, duplicados )
 
 sum(colnames(mat) %in% clinical$sample) #1032/1054 muestras de mat están en clinical
 
   #Cojo solo las muestras de la matriz de expresión que tengo en clinical
 snames<- intersect(colnames(mat), clinical$sample)
-sum(duplicated(clinical$sample)) #Vemos cuantos duplicados hay
+
+  #Vemos cuantos duplicados hay
+sum(duplicated(clinical$sample)) 
+
+  #Confirmamos que son duplicados
+clinical [duplicated(clinical$sample),]
+
+  #Me quedo con los que no son duplicados
+clinical<- clinical [!duplicated(clinical$sample),]
+
+  #Me quedo con los que están tanto en clínical como en matriz
+clinical<- clinical[snames,]
+matriz <- mat[,snames]
 
 
 
